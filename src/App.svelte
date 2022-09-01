d12909c04275061c6f2ae9bb4ad05705bc18e86f
kekelberi <alexei.gaponov@gmail.com>
2022-09-01 17:30:46 +0300

    save

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,18 @@
 <script>
-	export let name, age;
+  import Addtodo from './components/AddTodo.svelte'
+  export let name, age
 </script>
 
 <main>
-	<h1>Hello {name} {age}!</h1>
+  <h1>Hello {name} {age}!</h1>
+  <Addtodo />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 </style>
---
+++ src/components/AddTodo.svelte
@@ -0,0 +1,16 @@
+<div class="main_conteiner">
+  <input class="todo-input" />
+  <button class="">Add Todo</button>
+  
+</div>
+
+<style>
+  .main_conteiner {
+    background-color: aqua;
+    border-radius: 5px;
+    padding: 5px;
+  }
+  .todo-input {
+    color: blueviolet;
+  }
+</style>
